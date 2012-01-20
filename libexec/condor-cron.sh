286ffad97f17dead0ca7ec2930bdeed5421e5989
Scot Kronenfeld <kronenfe@cs.wisc.edu>
2012-01-20 21:05:06 +0000

    Have condor-cron.sh source /etc/sysconfig/condor-cron where we will do any necessary dynamic environment setup.  Currently this environment setup consists of adding non-standard Condor installations to the PATH
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/condor-cron/trunk@14017 4e558342-562e-0410-864c-e07659590f8c

--- libexec/condor-cron.sh
+++ libexec/condor-cron.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+# Pick up any configuration for condor-cron from /etc/sysconfig.
+# The big thing we are looking for is that Condor can sometimes be installed
+# into a non-standard location and we need to set it up in the PATH
+if [ -f /etc/sysconfig/condor-cron ]; then
+    . /etc/sysconfig/condor-cron
+fi
+
 CONDOR_CONFIG=/etc/condor-cron/condor_config
 export CONDOR_CONFIG
